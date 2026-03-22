f041ab0ca91fd727cc1abcb592289ac1032328b2
WahyuFZP <wfzputra@gmail.com>
2026-03-22 11:50:43 +0700

    feat: initial commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import { Components } from 'react';
+
+class Header extends Components {
+    render() {
+        return (
+            <div>
+                <h1>Kemitraan Terak Lele</h1>
+            </div>
+        );
+    }
+}
